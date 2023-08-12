dada8887557de672dca9fc901c6f3379c517df96
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-12 14:46:21 +0200

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+    
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    
+    let result = "";
 
+    for (let i = 0; i < repetitions; i++) {
+        result = result + string;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
