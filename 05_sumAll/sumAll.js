98666ce643fbbde108632976ff39e846fcb6cbf3
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-12 17:51:54 +0200

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR";
+    }
 
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+
+    if (startNum > endNum) {
+        let temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+
+    let finalSum = 0;
+    let currentNum = startNum;
+    while (currentNum <= endNum) {
+        finalSum += currentNum;
+        currentNum++;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
