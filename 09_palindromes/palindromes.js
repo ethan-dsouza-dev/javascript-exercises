1d7ae3311c744fb98db5588506fc33329c272ca2
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-25 19:30:13 +0200

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let regex = /[^\w\s]|_/g
+    let forward = word.toLowerCase().replaceAll(regex, '');
+    forward = forward.replaceAll(" ", "");
 
+    return Array.from(forward).reverse().join("") === forward;
+    
 };
 
 // Do not edit below this line
