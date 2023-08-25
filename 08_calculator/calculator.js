c532bf770096d5e451796beed204353b259e590d
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-25 15:25:13 +0200

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	arr.forEach((num) => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+	arr.forEach((num) => {
+    product *= num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, index) {
+	return base ** index;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+
+  let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
