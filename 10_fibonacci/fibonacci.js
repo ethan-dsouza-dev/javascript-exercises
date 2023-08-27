27843df347bd59c10984b00b557b0ae569ebc922
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-27 23:43:25 +0200

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(nTerm) {
+    if (nTerm <= 0) return "OOPS";
+    if (typeof nTerm === "string") nTerm = Number(nTerm);
+    if (nTerm === 1 || nTerm === 2) return 1;
 
+    return fibonacci(nTerm - 1) + fibonacci(nTerm -2);
 };
 
 // Do not edit below this line
